9107d3608a4afca5d43146a23e002b1408e4424d
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-29 09:22:16 -0700

    feat: intial bot login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello EddieHub!");
+import { Client } from "discord.js";
+
+import { IntentOptions } from "./config/IntentOptions";
+
+(async () => {
+  const bot = new Client({ intents: IntentOptions });
+
+  bot.on("ready", () => {
+    console.log(`Logged in as ${bot.user?.tag}!`);
+  });
+
+  await bot.login(process.env.TOKEN);
+})();
